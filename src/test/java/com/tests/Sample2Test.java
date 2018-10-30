7c460d9123af8c7ba31e6bb62c389ab595df905e
vikas-thange-twc <vikas.thange@weather.com>
2018-10-29 23:18:48 -0400

    initial commit

---
+++ src/test/java/com/tests/Sample2Test.java
@@ -0,0 +1,26 @@
+package com.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Sample2Test {
+
+	@Test
+	public void test1Pass() {
+		
+	}
+	@Test
+	public void test2Fail() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test3Pass() {
+		
+	}
+	@Test
+	public void test4Fail() {
+		fail("ABCDException");
+	}
+
+}
